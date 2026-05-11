482d9f9c31c29231b71616e932a79befd7165aa0
mumiwasnotavailable <themumke@gmail.com>
2026-05-11 19:49:05 +0200

    Dan 6 mini frontend projekt

--- script.js
+++ script.js
@@ -1,11 +1,58 @@
-const dugme = document.getElementById("dugme");
+const sportasi = [
 
-const naslov = document.getElementById("naslov");
+    {
+        ime: "Cristiano Ronaldo",
+        sport: "Fudbal",
+        opis: "Jedan od najboljih fudbalera svih vremena."
+    },
 
-dugme.addEventListener("click", function () {
+    {
+        ime: "LeBron James",
+        sport: "Košarka",
+        opis: "NBA legenda i višestruki prvak."
+    },
 
-    naslov.textContent = "JavaScript radi!";
+    {
+        ime: "Novak Djokovic",
+        sport: "Tenis",
+        opis: "Jedan od najuspješnijih tenisera ikad."
+    }
 
-    document.body.style.backgroundColor = "#d1e7ff";
+];
+
+const kartice = document.getElementById("kartice");
+
+const detalji = document.getElementById("detalji");
+
+sportasi.forEach(function (sportas) {
+
+    kartice.innerHTML += `
+
+        <div class="col-md-4 mb-4">
+
+            <div class="card p-3 h-100">
+
+                <h3>${sportas.ime}</h3>
+
+                <p>${sportas.sport}</p>
+
+                <button class="btn btn-primary"
+                    onclick="prikaziDetalje('${sportas.opis}')">
+
+                    Detalji
+
+                </button>
+
+            </div>
+
+        </div>
+
+    `;
 
 });
+
+function prikaziDetalje(opis) {
+
+    detalji.textContent = opis;
+
+}
